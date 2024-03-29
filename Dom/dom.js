faa67bd4760c676c6467b5d98878def1df535237
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-03-29 10:55:30 +0530

    first commit

---
+++ Dom/dom.js
@@ -0,0 +1,13 @@
+const headiing = document.querySelector('.second');
+const para = document.querySelector('#para');
+console.log(heading);
+console.log(para);
+console.log(document.getElementById("para"));
+console.log(
+    document.getElementsByClassName('first')
+);
+
+console.log(document.getElementsByTagName('h2'));
+
+heading.style.color= 'red';
+para.style.backgroundColor='grey';
