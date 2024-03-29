8348a19b4c349e368948177f7258303cc774b6d4
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-03-29 17:40:52 +0530

    third commit

--- Dom/dom.js
+++ Dom/dom.js
@@ -11,3 +11,27 @@ console.log(document.getElementsByTagName('h2'));
 
 heading.style.color= 'red';
 para.style.backgroundColor='grey';
+
+
+{/* <div style =  "background-color : yellow;">
+    <div style = "background-color: yellowgreen;" id ="innerDiv">
+        <h3 style = "background-color: green;"> Inner Heading </h3>
+
+
+    </div> */}
+// </div>
+
+
+document
+.querySelector('h3')
+.addEventListener('h3')
+.addEventListener('click',() =>{
+    console.log('Heading 3 clicked')
+});
+
+
+document 
+.getElementById('innerDiv')
+.addEventListener('click', () =>{
+    console.log("inner div clicked")
+});
---
+++ Dom/dom1.js
@@ -0,0 +1,43 @@
+createElement();
+
+const btn = document.  createElement ("button");
+btn .textContext = "Click here";
+btn.className = "btn1";
+
+
+const division = document. querySelector("div");
+division.appendChild(btn);
+
+//document.querySelector('h2').remove();
+
+const p = document.querySelector('p');
+p.addEventListener("click",function()
+{
+    clickPara();
+});
+
+// Event Listener on Para 
+;
+
+const p = document.querySelector('p')
+p.addEventListener('mouseover',function(){
+    p.style.backgroundColor = 'yellow'
+})
+p.addEventListener('mouseup',function(){
+    p.style.backgroundColor ='red'
+})
+
+
+document.body.addEventListener(
+    'keydown'
+    (event) => {
+        console.log ('keypressed');
+        console.log(event.keyCode);
+
+    }
+
+)
+function clickPara() {
+
+    console.log("para clicked ")
+}
